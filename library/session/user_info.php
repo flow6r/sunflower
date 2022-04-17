7a80211d3941074a63863c1b46d5f62157822d28
m66rk9t <m66rk9t@gmail.com>
2022-04-17 20:54:33 +0800

    Upload library/session/user_info.php

---
+++ library/session/user_info.php
@@ -0,0 +1,22 @@
+<?php
+/*储存用户信息会话的脚本*/
+//启动会话
+session_start();
+//检查专业信息字段
+if(empty($MjrAbrv)) $MjrName = null;
+//声明并初始化用户信息数组
+$userInfo = array(
+    "UsrID" => $UsrID,
+    "UsrName" => $UsrName,
+    "UsrGen" => $UsrGen,
+    "UsrRole" => $UsrRole,
+    "UsrEmail" => $UsrEmail,
+    "UsrAdms" => $UsrAdms,
+    "ColgAbrv" => $ColgAbrv,
+    "MjrAbrv" => $MjrAbrv,
+    "ColgName" => $ColgName,
+    "MjrName" => $MjrName,
+);
+//创建会话变量
+$_SESSION["userInfo"] = $userInfo;
+?>
