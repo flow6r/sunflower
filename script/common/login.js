deb515290d46bdb0b668b1a4350fd4b6e909b425
m66rk9t <m66rk9t@gmail.com>
2022-04-17 13:12:40 +0800

    Upload script/common/login.js

---
+++ script/common/login.js
@@ -0,0 +1,47 @@
+$("#loginDiv").on("focusout", "#usrID", function () {
+    let usrID = $("#loginDiv").find("#usrID").val();
+
+    if (usrID === "") $("#loginDiv").find("#usrID").attr("placeholder", "请输入用户ID");
+    else $("#loginDiv").find("#usrID").removeAttr("placeholder");
+});
+
+$("#loginDiv").on("focusout", "#usrPasswd", function () {
+    let usrPasswd = $("#loginDiv").find("#usrPasswd").val();
+
+    if (usrPasswd === "") $("#loginDiv").find("#usrPasswd").attr("placeholder", "请输入密码");
+    else $("#loginDiv").find("#usrPasswd").removeAttr("placeholder");
+});
+
+$("#loginDiv").on("click", "#loginBtn", function () {
+    let usrID = $("#loginDiv").find("#usrID").val();
+    let usrPasswd = $("#loginDiv").find("#usrPasswd").val();
+
+    if (usrID === "" || usrPasswd === "") {
+        $("#loginDiv").find("#tips").attr("style", "visibility: visible;");
+        $("#loginDiv").find("#usrID").attr("placeholder", "请输入用户ID");
+        if (usrID === "" && usrPasswd === "") {
+            $("#loginDiv").find("#usrPasswd").attr("placeholder", "请输入密码");
+            $("#loginDiv").find("#tips").attr("placeholder", "请完善登录信息");
+        }
+    }
+    else {
+        $.ajax({
+            url: "../../library/common/check_login.php",
+            type: "POST",
+            async: false,
+            data: { usrID: usrID, usrPasswd: usrPasswd },
+            error: function () { alert("查询数据库失败，请联系管理员并反馈问题"); },
+            success: function (status) {
+                if (status === "successful") {
+                    alert("登录成功");
+                } else {
+                    if (status.length > 6) alert(status);
+                    else {
+                        $("#loginDiv").find("#tips").attr("placeholder", status);
+                        $("#loginDiv").find("#tips").attr("style", "visibility: visible;");
+                    }
+                }
+            }
+        });
+    }
+});
