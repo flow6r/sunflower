bfa18d55622c147fd6b19bb0ca59dedbe3c4cf58
m66rk9t <m66rk9t@gmail.com>
2022-04-18 19:09:44 +0800

    Update library/session/user_info.php

--- library/session/user_info.php
+++ library/session/user_info.php
@@ -3,9 +3,6 @@
 //启动会话
 session_start();
 
-//检查专业信息字段
-if(empty($MjrAbrv)) $MjrName = null;
-
 //声明并初始化用户信息数组
 $userInfo = array(
     "UsrID" => $UsrID,
@@ -16,8 +13,6 @@ $userInfo = array(
     "UsrAdms" => $UsrAdms,
     "ColgAbrv" => $ColgAbrv,
     "MjrAbrv" => $MjrAbrv,
-    "ColgName" => $ColgName,
-    "MjrName" => $MjrName,
 );
 
 //创建会话变量
