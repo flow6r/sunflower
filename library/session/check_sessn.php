40fa8be889b4e610b114e062adad496af8ff4588
m66rk9t <m66rk9t@gmail.com>
2022-04-19 18:48:11 +0800

    Update library/common/check_sessn.php

--- library/session/check_sessn.php
+++ library/session/check_sessn.php
@@ -6,7 +6,7 @@ session_start();
 //检查会话变量是否存在
 if (!isset($_SESSION["userInfo"])) {
     //返回错误信息
-    $error = ["error"=>"启动会话时发生错误，请联系管理员并反馈问题"];
+    $error = ["error" => "启动会话时发生错误，请联系管理员并反馈问题"];
     $errorJSON = json_encode($error, JSON_UNESCAPED_UNICODE);
     echo $errorJSON;
 } else {
