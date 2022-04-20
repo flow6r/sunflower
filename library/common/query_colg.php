d4d93ff3bfce38d07199d9596ba0fe9fe372165e
m66rk9t <m66rk9t@gmail.com>
2022-04-20 16:33:38 +0800

    Update library/common/query_colg.php

--- library/common/query_colg.php
+++ library/common/query_colg.php
@@ -1,7 +1,23 @@
 <?php
 /*查询学院信息的脚本*/
+//获取GET请求的数据
+$usrRole = $_GET["usrRole"];
+
 //引入数据库用户信息脚本
-require_once("../dbuser/student.php");
+switch ($usrRole) {
+    case "std":
+        require_once("../dbuser/student.php");
+        break;
+    case "tch":
+        require_once("../dbuser/teacher.php");
+        break;
+    case "admin":
+        require_once("../dbuser/admin.php");
+        break;
+    default:
+        require_once("../dbuser/temp.php");
+        break;
+}
 
 //连接数据库
 $db = mysqli_connect($dbServer, $dbUser, $dbUserPasswd, $dbName);
