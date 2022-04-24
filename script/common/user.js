bbc88c26eee6c6bb25036e9cf5c96f65e853e0f9
m66rk9t <m66rk9t@gmail.com>
2022-04-24 18:33:56 +0800

    Update script/common/user.js

--- script/common/user.js
+++ script/common/user.js
@@ -160,7 +160,7 @@ $("#content").on("click", "#usrMgtDiv #qryUsrMenuTbl #addRecBtn", function () {
     );
 
     if (trgtRole === "tch") {
-        $("body").find("#addRecDiv").find("#addRecFrm").find("#addRecTbl").find("#addRecTitl").empty().append("新增教师用户记录");
+        $("body").find("#addRecDiv").find("#addRecFrm").find("#addRecTbl").find("#addRecTitl").empty().append("新增教师用户");
         $("body").find("#addRecDiv").find("#addRecFrm").find("#addRecTbl").find("#optRow").remove();
         $("body").find("#addRecDiv").attr("style", "height: 400px;");
         $("body").find("#addRecDiv").find("#addRecFrm").attr("style", "height: 400px;");
@@ -261,8 +261,10 @@ $("body").on("click", "#addRecDiv #addRecFrm #addRecTbl #addNewRecBtn", function
                     if (status === "successful") alert("成功添加新的用户记录");
                     else {
                         $("body").find(".usrMgtPopupTips").attr("style", "visibility: visible;");
-                        $("body").find(".usrMgtPopupTips").find("span").append(status);                   
+                        $("body").find(".usrMgtPopupTips").find("span").append(status);
                     }
+
+                    ReQueryUsrs();
                 }
             });
         }
@@ -271,6 +273,43 @@ $("body").on("click", "#addRecDiv #addRecFrm #addRecTbl #addNewRecBtn", function
         $("body").find(".usrMgtPopupTips").find("span").append("请完善用户信息");
     }
 });
+
+/*显示批量添加用户记录的弹窗*/
+$("#content").on("click", "#usrMgtDiv #qryUsrMenuTbl #impRecsBtn", function () {
+    $("#mask").attr("style", "visibility: visible;");
+    $("body").append(
+        "<div id='impRecsDiv' name='impRecsDiv' class='usrMgtPopup'>" +
+        "<form id='impRecsFrm' name='impRecsFrm' enctype='multipart/form-data' " +
+        "action='../../library/common/import_stds.php' method='post' target='doNotRefresh' onsubmit='return checkTmplFile()'>" +
+        "<table id='impRecsTbl' name='impRecsTbl'><tr><th colspan='2'><span>批量导入学生用户</span></th></tr>" +
+        "<tr><th colspan='2'><a href='http://localhost/data/tmpl/StdUsrInfoTmpl.xlsx'>下载模板文件</a></th></tr>" +
+        "<tr><td><label>用户信息文件</label></td><td><input type='file' id='usrInfoTmplFile' name='usrInfoTmplFile' /></td></tr>" +
+        "<tr><td><input type='button' class='cnlBtn' value='取消' /></td>" +
+        "<td><input type='submit' name='' class='impNewRecsBtn' value='导入' /></td></tr></table></form>" +
+        "<iframe id='doNotRefresh' name='doNotRefresh' title='doNotRefresh' style='display: none;'></iframe></div>"
+    );
+
+    if (trgtRole === "tch") {
+        $("body").find("#impRecsDiv").find("#impRecsFrm").find("#impRecsTbl").find("span").empty();
+        $("body").find("#impRecsDiv").find("#impRecsFrm").find("#impRecsTbl").find("span").append("批量导入教师用户");
+        $("body").find("#impRecsDiv").find("#impRecsFrm").attr("action", "../../library/common/import_tchs.php");
+        $("body").find("#impRecsDiv").find("#impRecsFrm").find("#impRecsTbl").find("a").attr(
+            "href", "http://localhost/data/tmpl/TchUsrInfoTmpl.xlsx"
+        );
+    }
+});
+
+/*实现检查模板文件的函数*/
+function checkTmplFile() {
+    let usrInfoTmplFile = $("body").find("#impRecsDiv").find("#impRecsFrm").find("#impRecsTbl").find("#usrInfoTmplFile").val();
+
+    if (usrInfoTmplFile === "") {
+        alert("请选择待导入的用户信息文件后再执行导入操作");
+        return false;
+    }
+
+    return true;
+}
 
 /*批量删除用户记录*/
 $("#content").on("click", "#usrMgtDiv #qryUsrMenuTbl #delRecsBtn", function () {
@@ -284,6 +323,8 @@ $("#content").on("click", "#usrMgtDiv #qryUsrMenuTbl #delRecsBtn", function () {
             success: function (status) {
                 if (status === "successful") alert("成功删除" + usrIDAray.length + "条用户记录");
                 else alert(status);
+
+                ReQueryUsrs();
             }
         });
     } else alert("您选择了0条用户记录，请选择至少一条记录后再执行批量删除操作");
@@ -291,8 +332,27 @@ $("#content").on("click", "#usrMgtDiv #qryUsrMenuTbl #delRecsBtn", function () {
 
 /*关闭弹窗*/
 $("body").on("click", ".usrMgtPopup .cnlBtn", function () {
+    ReQueryUsrs();
+});
+
+/*实现重新查询的函数*/
+function ReQueryUsrs() {
     $("#mask").attr("style", "visibility: hidden;");
 
     $("body").find(".usrMgtPopup").remove();
     $("body").find(".usrMgtPopupTips").remove();
-});
+
+    let searchItem = $("#content").find("#usrMgtDiv").find("#qryUsrMenuTbl").find("#qryUsrItem").val();
+    let searchType = $("#content").find("#usrMgtDiv").find("#qryUsrMenuTbl").find("#qryUsrType").val();
+
+    $("#content").find("#usrMgtDiv").find("#qryUsrBarTbl").find("#qryUsrAnchor").siblings().remove();
+    $("#content").find("#usrMgtDiv").find(".qryRecsLstTbl").find("#usrRecsHead").siblings().remove();
+
+    if (searchItem === "") {
+        searchItem = "";
+        searchType = "UsrID";
+    }
+
+    if (usrInfo["UsrRole"] === "tch") queryUsrs(usrInfo["UsrRole"], usrInfo["ColgAbrv"], usrInfo["MjrAbrv"], trgtRole, usrInfo["MjrAbrv"], "MjrAbrv");
+    else queryUsrs(usrInfo["UsrRole"], usrInfo["ColgAbrv"], usrInfo["MjrAbrv"], trgtRole, searchItem, searchType);
+}
