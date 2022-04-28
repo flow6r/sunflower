b8926198311ebe19a25088a09f574e8716a1cf76
m66rk9t <m66rk9t@gmail.com>
2022-04-28 17:44:18 +0800

    Update script/common/course.js

--- script/common/course.js
+++ script/common/course.js
@@ -265,3 +265,189 @@ function ReQueryCrses() {
     queryCrses(usrInfo["UsrID"], usrInfo["UsrRole"], usrInfo["ColgAbrv"], usrInfo["MjrAbrv"], searchItem, searchType);
 }
 
+/*进入课程详情页*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv .crseDetlAnchor", function (event) {
+    let crseID = $(event.target).attr("id");
+
+    queryCurrCrseInfo(crseID);
+});
+
+/*实现查询课程详情的函数*/
+function queryCurrCrseInfo(crseID) {
+    $.ajax({
+        url: "../../library/common/query_crse.php",
+        type: "GET",
+        async: false,
+        data: { crseID: crseID, usrRole: usrInfo["UsrRole"] },
+        dataType: "json",
+        error: function () { alert("查询数据库失败，请联系管理员并反馈问题"); },
+        success: function (crseJSON) {
+            if (crseJSON["error"] === "查询课程失败，请联系管理员并反馈问题") alert("查询课程失败，请联系管理员并反馈问题");
+            else {
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find("#qryCrseMenuTbl").find("input").attr("disabled", "disabled");
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find("#qryCrseMenuTbl").find("select").attr("disabled", "disabled");
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find("#qryCrseBarTbl").find("#qryCrseAnchor").siblings().remove();
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find("#qryCrseBarTbl").find("#qryCrseAnchor").after("<a id='crseInfoAnchor' class='" + crseID + "' href='#'>课程详情&gt;</a>");
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".recsPageCtlTbl").attr("style", "visibility: hidden;");
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").empty();
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").append(
+                    "<table id='currCrseInfoTbl'><tr><td rowspan='4'><img src='' /></td>" +
+                    "<td><label>课程名称</label></td><td><input type='text' id='crseName' name='crseName' maxlength='20' disabled='disabled' /></td>" +
+                    "</tr><tr><td><label>讲师</label></td><td><input type='text' id='usrName' name='usrName' maxlength='20' disabled='disabled' /></td>" +
+                    "</tr><tr><td><label>课程描述</label></td><td><textarea id='crseDesc' placeholder='' disabled='disabled'></textarea></td>" +
+                    "</tr><tr><td colspan='2'><a id='qryStds' href='#'>选课学生</a>&nbsp;<a id='qryMss' href='#'>课程作业</a>&nbsp;" +
+                    "<a id='addMss' href='#'>布置作业</a></td></tr><tr><td><input type='button' id='updtCover' name='" + crseJSON[0].CrseID + "' value='更新封面' /></td>" +
+                    "<td><input type='button' id='editCrseInfoBtn' name='editCrseInfoBtn' value='编辑' /></td>" +
+                    "<td colspan='2'><input type='button' id='cnlEditCrseInfoBtn' name='" + crseJSON[0].CrseID + "' value='取消' />" +
+                    "<input type='button' id='updtCrseInfoBtn' name='" + crseJSON[0].CrseID + "' value='更新' /></td></tr></table>"
+                );
+
+                if (usrInfo["UsrRole"] === "std") {
+                    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#updtCover").attr("id", "regCrseBtn");
+                    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#regCrseBtn").attr("value", "立即报名");
+                    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#addMss").remove();
+                    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#editCrseInfoBtn").remove();
+                    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#cnlEditCrseInfoBtn").remove();
+                    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#updtCrseInfoBtn").remove();
+                }
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("img").attr(
+                    "src", (crseJSON[0].CoverPath === null ? "../image/crsefront/temp/meerkat.jpg" : crseJSON[0].CoverPath));
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").attr("placeholder", crseJSON[0].CrseName);
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").attr("placeholder", crseJSON[0].UsrName);
+                $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").attr("placeholder", crseJSON[0].CrseDesc);
+            }
+        }
+    });
+}
+
+/*通过导航栏查询课程详情*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm #qryCrseBarTbl #crseInfoAnchor", function (event) {
+    let crseID = $(event.target).attr("class");
+
+    queryCurrCrseInfo(crseID);
+});
+
+/*编辑课程信息*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #editCrseInfoBtn", function () {
+    let crseName = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").attr("placeholder");
+    let usrName = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").attr("placeholder");
+    let crseDesc = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").attr("placeholder");
+
+    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").removeAttr("placeholder").removeAttr("disabled").val(crseName);
+    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").removeAttr("placeholder").removeAttr("disabled").val(usrName);
+    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").removeAttr("placeholder").removeAttr("disabled").val(crseDesc);
+
+    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#editCrseInfoBtn").attr("style", "visibility: hidden;");
+    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#cnlEditCrseInfoBtn").attr("style", "visibility: visible;");
+    $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#updtCrseInfoBtn").attr("style", "visibility: visible;");
+});
+
+/*显示更新课程封面的弹窗*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #updtCover", function (event) {
+    let crseID = $(event.target).attr("name");
+
+    $("#mask").attr("style", "visibility: visible;");
+    $("body").append(
+        "<div id='updtCoverDiv' name='updtCoverDiv'>" +
+        "<form id='updtCoverFrm' name='updtCoverFrm' enctype='multipart/form-data' " +
+        "action='../../library/common/update_cover.php' method='post' target='doNotRefresh' onsubmit='return checkCover()'>" +
+        "<table id='updtCoverTbl' name='updtCoverTbl'><tr><th colspan='2'><span>上传封面</span></th></tr>" +
+        "<tr><td><label>封面文件</label></td><td><input type='file' id='newCover' name='newCover' /></td></tr>" +
+        "<tr><td><input type='button' id='cnlUpdtCoverBtn' name='" + crseID + "' value='取消' /></td>" +
+        "<td><input type='submit' id='updtCoverBtn' name='" + crseID + "' value='上传' /></td></tr></table></form>" +
+        "<iframe id='doNotRefresh' name='doNotRefresh' title='doNotRefresh' style='display: none;'></iframe></div>"
+    );
+});
+
+/*检查封面图片文件的函数*/
+function checkCover() {
+    let newCover = $("body").find("#updtCoverDiv").find("#updtCoverFrm").find("#updtCoverTbl").find("#newCover").val();
+
+    if (newCover == "") { alert("请选择待上传的封面图片文件后再执行上传操作"); return false; }
+
+    return true;
+}
+
+/*实现取消更新封面的函数*/
+$("body").on("click", "#updtCoverDiv #updtCoverFrm #updtCoverTbl #cnlUpdtCoverBtn", function (event) {
+    let crseID = $(event.target).attr("name");
+
+    $("#mask").attr("style", "visibility: hidden;");
+
+    $("body").find("#updtCoverDiv").remove();
+
+    queryCurrCrseInfo(crseID);
+});
+
+/*取消更新课程信息*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #cnlEditCrseInfoBtn", function (event) {
+    let crseID = $(event.target).attr("name");
+
+    queryCurrCrseInfo(crseID);
+});
+
+/*更新课程信息时检查课程名称完整性*/
+$("#content").on("focusout", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #crseName", function () {
+    let crseName = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").val();
+
+    if (crseName == "") $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").attr("placeholder", "请输入课程名称");
+    else $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").removeAttr("placeholder");
+});
+
+/*更新课程信息时检查讲师姓名完整性*/
+$("#content").on("focusout", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #usrName", function () {
+    let usrName = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").val();
+
+    if (usrName == "") $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").attr("placeholder", "请输入教师姓名");
+    else $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").removeAttr("placeholder");
+});
+
+
+/*更新课程信息时检查讲师描述完整性*/
+$("#content").on("focusout", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #crseDesc", function () {
+    let crseDesc = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").val();
+
+    if (crseDesc == "") $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").attr("placeholder", "请输入课程描述");
+    else $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").removeAttr("placeholder");
+});
+
+/*更新课程信息*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #updtCrseInfoBtn", function (event) {
+    let crseName = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseName").val();
+    let usrName = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#usrName").val();
+    let crseDesc = $("#content").find("#crseMgtDiv").find("#crseMgtFrm").find(".qryCrseRecsDiv").find("#currCrseInfoTbl").find("#crseDesc").val();
+    let crseID = $(event.target).attr("name");
+
+    if (crseName != "" && usrName != "" && crseDesc != "") {
+        $.ajax({
+            url: "../../library/common/update_crse.php",
+            type: "POST",
+            async: false,
+            data: { crseName: crseName, usrName: usrName, crseDesc: crseDesc, usrRole: usrInfo["UsrRole"], colgAbrv: usrInfo["ColgAbrv"] },
+            error: function () { alert("查询数据库失败，请联系管理员并反馈问题"); },
+            success: function (status) {
+                if (status === "successful") {
+                    alert("成功更新课程信息");
+                    queryCurrCrseInfo(crseID);
+                } else alert(status);
+            }
+        });
+    } else alert("请完善待更新的课程信息后再执行更新操作");
+});
+
+/*报名课程*/
+$("#content").on("click", "#crseMgtDiv #crseMgtFrm .qryCrseRecsDiv #currCrseInfoTbl #regCrseBtn", function (event) {
+    let crseID = $(event.target).attr("name");
+
+    $.ajax({
+        url: "../../library/common/course_reg.php",
+        type: "POST",
+        async: false,
+        data: { crseID: crseID, usrID: usrInfo["UsrID"] },
+        error: function () { alert("查询数据库失败，请联系管理员并反馈问题"); },
+        success: function (status) {
+            if (status === "successful") { alert("成功报名课程"); queryCurrCrseInfo(crseID); }
+            else alert(status);
+        }
+    });
+});
