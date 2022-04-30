d6d27cc1e2f5da2b694987dc586c5780b306c2fe
m66rk9t <m66rk9t@gmail.com>
2022-04-30 12:42:29 +0800

    Upload library/common/query_ptys.php

---
+++ library/common/query_ptys.php
@@ -0,0 +1,77 @@
+<?php
+/*查询学习小组记录的脚本*/
+//获取GET请求的数据
+$usrID = $_GET["usrID"];
+$usrRole = $_GET["usrRole"];
+$colgAbrv = $_GET["colgAbrv"];
+$mjrAbrv = $_GET["mjrAbrv"];
+$searchItem = $_GET["searchItem"];
+$searchType = $_GET["searchType"];
+
+//引入数据库用户信息脚本
+switch ($usrRole) {
+    case "std":
+        require_once("../dbuser/student.php");
+        break;
+    case "tch":
+        require_once("../dbuser/teacher.php");
+        break;
+    case "admin":
+        require_once("../dbuser/admin.php");
+        break;
+}
+
+//连接数据库
+$db = mysqli_connect($dbServer, $dbUser, $dbUserPasswd, $dbName);
+if (mysqli_connect_error()) {
+    echo "连接数据库失败，请联系管理员并反馈问题";
+    exit;
+}
+
+//设置查询关键词
+$searchItem = "%" . $searchItem . "%";
+
+//查询数据库
+switch ($usrRole) {
+    case "std":
+        if ($searchType === "UsrName") $query = "SELECT P.PtyID, P.PtyName, U.UsrName FROM User AS U, Party AS P, " .
+        "Member AS M WHERE M.UsrID = ? AND P.PtyID = M.PtyID AND P.UsrID = U.UsrID AND U." . $searchType . " LIKE ?";
+        else $query = "SELECT P.PtyID, P.PtyName, U.UsrName FROM User AS U, Party AS P, Member AS M " .
+        "WHERE M.UsrID = ? AND P.PtyID = M.PtyID AND P.UsrID = U.UsrID AND P." . $searchType . " LIKE ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("ss", $usrID, $searchItem);
+        break;
+    case "tch":
+        if ($searchType === "UsrName") $query = "SELECT P.PtyID, P.PtyName, U.UsrName FROM User AS U, Course AS C, " .
+        "Party AS P, Member AS M WHERE C.UsrID = ? AND C.CrseID = P.CrseID AND P.PtyID = M.PtyID AND P.UsrID = U.UsrID " .
+        "AND P.UsrID = M.UsrID AND U." . $searchType . " LIKE ?";
+        else $query = "SELECT P.PtyID, P.PtyName, U.UsrName FROM User AS U, Course AS C, Party AS P " .
+        "WHERE C.UsrID = ? AND C.CrseID = P.CrseID AND P.UsrID = U.UsrID AND P." . $searchType . " LIKE ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("ss", $usrID, $searchItem);
+        break;
+    case "admin":
+        if ($searchType === "UsrName") $query = "SELECT P.PtyID, P.PtyName, U.UsrName FROM User AS U, Party AS P, " .
+        "Member AS M WHERE P.PtyID = M.PtyID AND U.UsrID = M.UsrID AND U.ColgAbrv = ? AND P.UsrID = M.UsrID AND U." . $searchType . " LIKE ?";
+        else $query = "SELECT P.PtyID, P.PtyName, U.UsrName FROM User AS U, Party AS P, Member AS M " .
+        "WHERE P.PtyID = M.PtyID AND U.UsrID = M.UsrID AND U.ColgAbrv = ? AND P.UsrID = M.UsrID AND P." . $searchType . " LIKE ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("ss", $colgAbrv, $searchItem);
+        break;
+}
+$stmt->execute();
+
+//将查询结果以JSON数据格式返回给浏览器
+$result = $stmt->get_result();
+$ptys = $result->fetch_all(MYSQLI_ASSOC);
+$ptysJSON = json_encode($ptys, JSON_UNESCAPED_UNICODE);
+echo $ptysJSON;
+
+//将JSON数据写入文件
+// file_put_contents("ptys.json", $ptysJSON);
+
+//释放结果集并关闭链接
+$stmt->free_result();
+$db->close();
+exit;
+?>
